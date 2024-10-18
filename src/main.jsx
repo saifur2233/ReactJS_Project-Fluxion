3af6ea3c3e99bec8631f0688d900380b022ade08
saifur2233 <saifurnstuiit223344@gmail.com>
2024-10-18 20:29:34 +0600

    usercontext path changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import UserContext from './contexts/UserContext.jsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import UserContext from './contexts/UserContext.jsx';
 // Create a client
 const queryClient = new QueryClient();
 
